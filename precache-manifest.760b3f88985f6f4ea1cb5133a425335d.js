0fb99d40003a480d6354d057fd118cc18d026672
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-11-06 15:49:05 +0900

    Updates

---
+++ precache-manifest.760b3f88985f6f4ea1cb5133a425335d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc28645dfcee8ddf7ef8462a2499cd6",
+    "url": "/vhvkhoa-page/index.html"
+  },
+  {
+    "revision": "888644e9353250128454",
+    "url": "/vhvkhoa-page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "681b2eb9575cc35d74e0",
+    "url": "/vhvkhoa-page/static/js/2.2ca3a89e.chunk.js"
+  },
+  {
+    "revision": "888644e9353250128454",
+    "url": "/vhvkhoa-page/static/js/main.f1c19893.chunk.js"
+  },
+  {
+    "revision": "93780ffcf94a02fa5e89",
+    "url": "/vhvkhoa-page/static/js/runtime-main.8b86cffa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vhvkhoa-page/static/media/logo.25bf045c.svg"
+  }
+]);
