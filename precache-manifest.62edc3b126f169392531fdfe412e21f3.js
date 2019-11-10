1a18364a7baf2287396fb0cc2768892c4648f3a6
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-11-10 16:17:07 +0900

    Updates

---
+++ precache-manifest.62edc3b126f169392531fdfe412e21f3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6394411c5b4a4a71d33",
+    "url": "/vhvkhoa-page/static/js/main.63fb3517.chunk.js"
+  },
+  {
+    "revision": "b77b7378991a3b155538",
+    "url": "/vhvkhoa-page/static/js/runtime~main.37d90d66.js"
+  },
+  {
+    "revision": "ad6cc78bb6c9d9d58c53",
+    "url": "/vhvkhoa-page/static/js/2.8be64628.chunk.js"
+  },
+  {
+    "revision": "beb3f4e1711563549c967cfd8501bffb",
+    "url": "/vhvkhoa-page/static/media/father-son.beb3f4e1.png"
+  },
+  {
+    "revision": "6b8ec0b8f39dd0b090517a1ca02e29a7",
+    "url": "/vhvkhoa-page/static/media/post.6b8ec0b8.jpg"
+  },
+  {
+    "revision": "1445e56ccbd3a565250dc6619fbfbf47",
+    "url": "/vhvkhoa-page/static/media/search-results.1445e56c.jpg"
+  },
+  {
+    "revision": "83ded1b2061a641049792a4737c7e17e",
+    "url": "/vhvkhoa-page/static/media/fruit-bowl.83ded1b2.jpg"
+  },
+  {
+    "revision": "25cba2ef28c2d71ff2ef90f4a9d3b5f3",
+    "url": "/vhvkhoa-page/static/media/opening-page.25cba2ef.jpg"
+  },
+  {
+    "revision": "b8528e94d9e9508de56814d1cfd817f7",
+    "url": "/vhvkhoa-page/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vhvkhoa-page/precache-manifest.93855ee4683d0ee274aeea7d88bc6d48.js"
+  "/vhvkhoa-page/precache-manifest.62edc3b126f169392531fdfe412e21f3.js"
 );
 
 workbox.clientsClaim();
